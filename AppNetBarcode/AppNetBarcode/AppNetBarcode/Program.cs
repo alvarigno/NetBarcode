a0e87bdce38958c5bc517d46e6980ed61d41f657
Álvaro Emparán <alvaro.emparan@gmail.com>
2020-05-02 04:22:17 -0400

    create PDF but no valid

--- AppNetBarcode/AppNetBarcode/AppNetBarcode/Program.cs
+++ AppNetBarcode/AppNetBarcode/AppNetBarcode/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using Spire.Pdf;
 
 namespace AppNetBarcode
 {
@@ -12,11 +15,25 @@ namespace AppNetBarcode
 
             Console.ReadKey();
 
+
+
             var barcode = new Barcode("7359999155288", Type.EAN13, true, 1000, 500);
 
             barcode.SaveImageFile("./path", ImageFormat.Jpeg);
 
             var value1 = barcode.GetBase64Image();
+
+
+//            byte[] bytes = Convert.FromBase64String(value1);
+
+
+//System.IO.FileStream stream =
+//    new FileStream(Directory.GetCurrentDirectory() + "test.pdf", FileMode.CreateNew);
+//            System.IO.BinaryWriter writer =
+//                new BinaryWriter(stream);
+//            writer.Write(bytes, 0, bytes.Length);
+//            writer.Close();
+
 
 
             Console.WriteLine(value1);
